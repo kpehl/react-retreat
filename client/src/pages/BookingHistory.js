b2f16d13d61d97536f1a39384e5cbe2291e697c5
Damaris C <DCampos07@users.noreply.github.com>
2021-02-04 17:36:16 -0500

    completed booking history CSS

--- client/src/pages/BookingHistory.js
+++ client/src/pages/BookingHistory.js
@@ -31,26 +31,26 @@ function BookingHistory() {
 
   return (
     <>
-      <div class="container flex-row">
-        <Link to="/">
+      <div className="container flex-row">
+        <Link className="link" to="/">
           ← Back to Home
           </Link>
-        <div class="row">
-          <div class="card-booking">
+        <div className="row">
+          <div class="card-main px-2 py-2">
             {user && bookings ? (
               <>
-                <h2>Booking History for {user.firstName} {user.lastName}</h2>
-                <div className=" flex-row">
+                <h2 className="booking-h2">Booking History for {user.firstName} {user.lastName}</h2>
+                <div className="flex-row">
                   <p>If you need to change or cancel a confirmed booking, please contact our staff.</p>
                 </div>
                 {bookings.map(booking => (
-                  <div key={booking._id} className="my-2">
+                  <div key={booking._id} className="card-booking px-2 py-2 mb-3">
                     <h4>Confirmation Number: {booking._id}</h4>
                     <div>
                       <p><span>Reservation Dates: {new Date(parseInt(booking.bookingDateStart)).toLocaleDateString()} to {new Date(parseInt(booking.bookingDateEnd)).toLocaleDateString()}</span></p>
                       <p>Purchase Date: {new Date(parseInt(booking.purchaseDate)).toLocaleDateString()}</p>
                     </div>
-                    <div className="flex-row">
+                    <div className="card-booking px-2 py-2 mb-3">
                       {booking.rooms.map(({ _id, name, price }, index) => (
                         <div key={index} className="my-2">
                           <Link to={`/rooms/${_id}`}>
