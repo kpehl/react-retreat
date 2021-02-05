16253ca6157f9a7408ff7d660108ef63357813b3
Kathleen Pehl <kathleen.pehl@gmail.com>
2021-02-04 19:49:18 -0500

    style re-updates; removed emotion and dotenv from package.json and resinstalled all packages; re-fixed booking history cards

--- client/src/pages/BookingHistory.js
+++ client/src/pages/BookingHistory.js
@@ -37,40 +37,42 @@ function BookingHistory() {
 
   return (
     <>
-      <div className="container my-1">
-        <Link to="/">
+      <div className="container flex-row">
+        <Link className="link" to="/">
           ← Back to Home
           </Link>
-
-        {user && bookings ? (
-          <>
-            <h2>Booking History for {user.firstName} {user.lastName}</h2>
-            <div className="flex-row">
-                  <p>If you need to change or cancel a confirmed booking, please contact our staff.</p>
-            </div>
-            {bookings.map((booking, bookingIndex) => (
-              <div key={booking._id} className="my-2">
-                <h4>Confirmation Number: {booking._id}</h4>
+          <div className="row">
+            <div className="card-main px-2 py-2">
+              {user && bookings ? (
+              <>
+                <h2>Booking History for {user.firstName} {user.lastName}</h2>
                 <div className="flex-row">
-                {booking.rooms.map(({ _id, name, price}, index) => (
-                    <div key={index} className="my-2">
-                      <Link to={`/rooms/${_id}`}>
-                        <p>{name}</p>
-                      </Link>
-                      <div>
-                        <p>Total Nights: {duration[bookingIndex]}</p>
-                        <p><span>${totalCost[bookingIndex]}</span></p>
-                        <p><span>Reservation Dates: {new Date(parseInt(booking.bookingDateStart)).toLocaleDateString()} to {new Date(parseInt(booking.bookingDateEnd)).toLocaleDateString()}</span></p>  
-                        <p>Purchase Date: {new Date(parseInt(booking.purchaseDate)).toLocaleDateString()}</p>
-                      </div>
-                    </div>
-                ))}
+                  <p>If you need to change or cancel a confirmed booking, please contact our staff.</p>
                 </div>
-              </div>
-            ))}
-          </>
-        ) : null}
-
+                {bookings.map((booking, bookingIndex) => (
+                <div key={booking._id} className="my-2">
+                  <h4>Confirmation Number: {booking._id}</h4>
+                  <div className="card-booking px-2 py-2 mb-3">
+                  {booking.rooms.map(({ _id, name, price}, index) => (
+                    <div key={index} className="my-2">
+                        <Link to={`/rooms/${_id}`}>
+                          <p>{name}</p>
+                        </Link>
+                        <div>
+                          <p>Total Nights: {duration[bookingIndex]}</p>
+                          <p><span>${totalCost[bookingIndex]}</span></p>
+                          <p><span>Reservation Dates: {new Date(parseInt(booking.bookingDateStart)).toLocaleDateString()} to {new Date(parseInt(booking.bookingDateEnd)).toLocaleDateString()}</span></p>  
+                          <p>Purchase Date: {new Date(parseInt(booking.purchaseDate)).toLocaleDateString()}</p>
+                        </div>
+                    </div>
+                  ))}
+                  </div>
+                </div>
+              ))}
+            </>
+          ) : null}
+            </div>
+          </div>
       </div>
 
     </>)
